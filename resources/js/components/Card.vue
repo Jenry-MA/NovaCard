e27fe80d0e5480ee533ef15e2092784aa80ba07a
Jenry Mazariegos <jenrymazariegos@gmail.com>
2023-08-02 15:09:49 -0600

    Add files via upload

---
+++ resources/js/components/Card.vue
@@ -0,0 +1,27 @@
+<template>
+  <Card class="flex flex-col items-center justify-center">
+    <div class="px-3 py-3">
+      <DigitalClock />
+    </div>
+  </Card>
+</template>
+
+<script>
+import DigitalClock from "./DigitalClock.vue";
+export default {
+    props: [
+        'card',
+
+        // The following props are only available on resource detail cards...
+        // 'resource',
+        // 'resourceId',
+        // 'resourceName',
+    ],
+    components: {
+      DigitalClock
+    },
+    mounted() {
+    //
+    },
+}
+</script>
---
+++ resources/js/components/DigitalClock.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="">
+        <div class="w-auto lg:w-1/4 flex flex-col">
+            <p class="font-bold text-white">{{ currentTime.toLocaleTimeString() }}</p>
+            <p class="font-bold text-white">{{ currentTime.toLocaleDateString() }}</p>
+        </div>
+    </div>
+</template>
+<script>
+import { useCurrentTime } from '../useCurrentTime';
+export default {
+    name: "CurrentTimeExample",
+    setup() {
+        const { currentTime } = useCurrentTime();
+        console.log(currentTime.value);
+        return { currentTime };
+    },
+};
+</script>
